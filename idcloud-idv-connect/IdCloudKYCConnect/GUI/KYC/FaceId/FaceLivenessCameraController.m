5ad8fd2c61a024791226bd5a503aef5badaed0b1
Vladislav Vesely <vladislav.vesely@external.thalesgroup.com>
2020-02-20 17:03:47 +0100

    Acuant 11.3.1

--- idcloud-idv-connect/IdCloudKYCConnect/GUI/KYC/FaceId/FaceLivenessCameraController.m
+++ idcloud-idv-connect/IdCloudKYCConnect/GUI/KYC/FaceId/FaceLivenessCameraController.m
@@ -28,14 +28,15 @@ Please make sure to review our IdCloud documentation, including security guideli
 
 @property (nonatomic, strong) UIView                        *overlayView;
 @property (nonatomic, assign) BOOL                          captured;
-@property (nonatomic, strong) CIContext                     *context;
 @property (nonatomic, strong) FaceCaptureSession            *captureSession;
-@property (nonatomic, assign) UIDeviceOrientation           lastDeviceOrientation;
 @property (nonatomic, strong) AVCaptureVideoPreviewLayer    *videoPreviewLayer;
 @property (nonatomic, strong) CAShapeLayer                  *faceOval;
-@property (nonatomic, assign) CGRect                        messageBoundingRect;
+@property (nonatomic, strong) CATextLayer                   *blinkLabel;
+@property (nonatomic, assign) CGFloat                        currentFrameTime;
 
 @end
+
+#define FRAME_DURATION 0.1f
 
 @implementation FaceLivenessCameraController
 
@@ -43,6 +44,7 @@ Please make sure to review our IdCloud documentation, including security guideli
 
 - (id)init {
     if (self = [super init]) {
+        _currentFrameTime = .0f;
         self.modalPresentationStyle = UIModalPresentationFullScreen;
     }
     
@@ -56,13 +58,12 @@ Please make sure to review our IdCloud documentation, including security guideli
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-//    (UIApplication.shared.delegate as! AppDelegate).orientationLock = .portrait
+    [self startCameraView];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    _captured = NO;
     [super viewDidAppear:animated];
-    [self startCameraView];
+    _captured = NO;
 }
 
 - (BOOL)prefersStatusBarHidden {
@@ -77,80 +78,62 @@ Please make sure to review our IdCloud documentation, including security guideli
     }
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    AVCaptureConnection *previewLayerConnection = self.videoPreviewLayer.connection;
-    if (previewLayerConnection && previewLayerConnection.isVideoOrientationSupported) {
-        UIDevice *currentDevice = UIDevice.currentDevice;
-        UIDeviceOrientation orientation = currentDevice.orientation;
-        
-        switch (orientation) {
-            case UIDeviceOrientationLandscapeRight:
-                [self updatePreviewLayer:previewLayerConnection orientation:AVCaptureVideoOrientationLandscapeLeft];
-                break;
-            case UIDeviceOrientationLandscapeLeft:
-                [self updatePreviewLayer:previewLayerConnection orientation:AVCaptureVideoOrientationLandscapeRight];
-                break;
-            case UIDeviceOrientationPortraitUpsideDown:
-                [self updatePreviewLayer:previewLayerConnection orientation:AVCaptureVideoOrientationPortraitUpsideDown];
-                break;
-            default:
-                [self updatePreviewLayer:previewLayerConnection orientation:AVCaptureVideoOrientationPortrait];
-                break;
-        }
+    if (_videoPreviewLayer.connection) {
+        [self updatePreviewLayer:_videoPreviewLayer.connection orientation:AVCaptureVideoOrientationPortrait];
     }
 }
 
-- (void)viewWillTransitionToSize:(CGSize)size
-       withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
-        UIInterfaceOrientation orient = UIApplication.sharedApplication.statusBarOrientation;
-        switch (orient) {
-            case UIInterfaceOrientationPortrait:
-                NSLog(@"Portrait");
-                break;
-            case UIInterfaceOrientationLandscapeLeft:
-            case UIInterfaceOrientationLandscapeRight:
-                NSLog(@"Landscape");
-                break;
-            default:
-                NSLog(@"Anything But Portrait");
-                break;
-        }
-    } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
-        [self.captureSession stopRunning];
-        [self startCameraView];
-    }];
-    
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-}
-
 // MARK: - Private Helpers
-
-- (void)startCameraView {
-    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
-                                                                        mediaType:AVMediaTypeVideo
-                                                                         position:AVCaptureDevicePositionFront];
-    self.captureSession = [AcuantHGLiveness getFaceCaptureSessionWithDelegate:self
-                                                                captureDevice:captureDevice
-                                                                 previewWidth:self.view.layer.bounds.size.width
-                                                                previewHeight:self.view.layer.bounds.size.height];
-    self.videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:_captureSession];
-    _videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    _videoPreviewLayer.frame = self.view.layer.bounds;
-    [self addoverlay];
-    [self displayBlinkMessage];
-    [self.view.layer addSublayer:_videoPreviewLayer];
-    [_captureSession startRunning];
-}
 
 - (void)updatePreviewLayer:(AVCaptureConnection *)layer orientation:(AVCaptureVideoOrientation)orientation {
     _videoPreviewLayer.connection.videoOrientation = orientation;
 }
 
+- (void)startCameraView {
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithDeviceType:AVCaptureDeviceTypeBuiltInWideAngleCamera
+                                                                        mediaType:AVMediaTypeVideo
+                                                                         position:AVCaptureDevicePositionFront];
+    
+    self.captureSession = [AcuantHGLiveness getFaceCaptureSessionWithDelegate:self
+                                                                captureDevice:captureDevice];
+    [_captureSession start];
+
+    self.videoPreviewLayer = [AVCaptureVideoPreviewLayer layerWithSession:_captureSession];
+    _videoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _videoPreviewLayer.frame = self.view.layer.bounds;
+
+    [self addoverlay];
+    [self displayBlinkMessage];
+    
+    self.faceOval = [CAShapeLayer new];
+    _faceOval.fillColor = UIColor.clearColor.CGColor;
+    _faceOval.strokeColor = UIColor.greenColor.CGColor;
+    _faceOval.lineWidth = 5.f;
+    
+    [_videoPreviewLayer addSublayer:_faceOval];
+    [self.view.layer addSublayer:_videoPreviewLayer];
+}
+
+- (BOOL)shouldSkipFrame:(LiveFaceDetails *)liveFaceDetails faceType:(AcuantFaceType)faceType {
+    BOOL skipFrame = NO;
+    if(_currentFrameTime < 0 || (liveFaceDetails && liveFaceDetails.isLiveFace) || CFAbsoluteTimeGetCurrent() - _currentFrameTime >= FRAME_DURATION) {
+        _currentFrameTime = CFAbsoluteTimeGetCurrent();
+    }
+    else{
+        skipFrame = YES;
+    }
+    return skipFrame;
+}
+
 - (CGRect)getViewFrame {
-    return UIScreen.mainScreen.bounds;
+    return [UIScreen mainScreen].bounds;
 }
 
 - (CGSize)getViewFrameSize {
@@ -158,180 +141,143 @@ Please make sure to review our IdCloud documentation, including security guideli
 }
 
 - (CGRect)getTransparentRect {
+    CGRect retRect = CGRectZero;
     
-    CGFloat hSpace          = .75f;
-    CGFloat vSpace          = .75f;
-    CGRect  overlayRect     = [self getViewFrame];
-    CGFloat rectWidth       = overlayRect.size.width;
-    CGFloat rectHeight      = overlayRect.size.height;
-    BOOL    isLandscape     = UIDeviceOrientationIsLandscape(UIDevice.currentDevice.orientation);
+    CGFloat hSpace = .95f;
+    CGFloat vSpace = .75f;
     
-    if(isLandscape){
-        [self swap:&rectWidth second:&rectHeight];
-    }
-
     if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
-        if(isLandscape){
-            vSpace = .5f;
-        }
-    } else if (isLandscape) {
         hSpace = .75f;
-        vSpace = .45f;
-    } else {
-        hSpace = .95f;
     }
+    CGRect overlayRect = [self getViewFrame];
+
+    CGFloat rectWidth = overlayRect.size.width;
+    CGFloat rectHeight = overlayRect.size.height;
+
+    CGFloat width = rectWidth * hSpace;
+    CGFloat height = rectHeight * vSpace;
+    CGFloat horizontalSpace = (overlayRect.size.width - width) * .5f;
+    CGFloat verticalSpace = .15f * overlayRect.size.height;
     
-    CGFloat width           = rectWidth * hSpace;
-    CGFloat height          = rectHeight * vSpace;
-    CGFloat horizontalSpace = (overlayRect.size.width-width) / 2.f;
-    CGFloat verticalSpace   = .15f * overlayRect.size.height;
+    retRect = CGRectMake(horizontalSpace, verticalSpace, width, height);
     
-    return CGRectMake(horizontalSpace, verticalSpace, width, height);
+    return retRect;
 }
+
 
 - (UIBezierPath *)getTransparentBezierPath {
     return [UIBezierPath bezierPathWithOvalInRect:[self getTransparentRect]];
 }
 
 - (void)addoverlay {
-    UIView *overlayView = [[UIView alloc] initWithFrame:[self getViewFrame]];
-    overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:.1f];
-    UIBezierPath *overlayPath = [UIBezierPath bezierPathWithRect:overlayView.bounds];
+    self.overlayView = [[UIView alloc] initWithFrame:[self getViewFrame]];
+    _overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:.1f];
+    UIBezierPath *overlayPath = [UIBezierPath bezierPathWithRect:_overlayView.bounds];
     UIBezierPath *transparentPath = [self getTransparentBezierPath];
     [overlayPath appendPath:transparentPath];
     overlayPath.usesEvenOddFillRule = YES;
-    CAShapeLayer *fillLayer = [CAShapeLayer new];
+    CAShapeLayer *fillLayer = [CAShapeLayer layer];
     fillLayer.path = overlayPath.CGPath;
     fillLayer.fillRule = kCAFillRuleEvenOdd;
     fillLayer.fillColor = [UIColor colorWithRed:.0f green:.0f blue:.0f alpha:.6f].CGColor;
-
     [_overlayView.layer addSublayer:fillLayer];
     [_videoPreviewLayer addSublayer:_overlayView.layer];
 }
 
-- (void)displayBlinkMessage {
-    NSMutableAttributedString *message = [[NSMutableAttributedString alloc] initWithString:@"Move closer and blink\nwhen green oval appears"];
-    [message addAttribute:NSForegroundColorAttributeName value:UIColor.whiteColor range:NSMakeRange(0, message.length)];
-    [message addAttribute:NSForegroundColorAttributeName value:UIColor.greenColor range:NSMakeRange(27, 10)];
-    [message addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:24.f] range:NSMakeRange(0, message.length)];
+- (void)addMessage {
+    [self addMessage:nil];
+}
 
-    CATextLayer *blinkLabel = [CATextLayer new];
-    blinkLabel.frame            = [self getBlinkMessageRect];
-    blinkLabel.string           = message;
-    blinkLabel.contentsScale    = UIScreen.mainScreen.scale;
-    blinkLabel.alignmentMode    = kCAAlignmentCenter;
-    blinkLabel.foregroundColor  = UIColor.whiteColor.CGColor;
-    
-    [_videoPreviewLayer addSublayer:blinkLabel];
+- (void)addMessage:(NSString *)message  {
+    [self addMessage:message color:UIColor.redColor.CGColor];
+}
+
+- (void)addMessage:(NSString *)message color:(CGColorRef)color  {
+    [self addMessage:message color:color fontSize:25.f];
+}
+
+- (void)addMessage:(NSString *)message color:(CGColorRef)color fontSize:(CGFloat)fontSize {
+    if(!message){
+        NSMutableAttributedString *msg = [[NSMutableAttributedString alloc] initWithString:@"Align face and blink when green oval appears"];
+        [msg addAttribute:NSForegroundColorAttributeName value:UIColor.whiteColor range:NSMakeRange(0, msg.length)];
+        [msg addAttribute:NSForegroundColorAttributeName value:UIColor.greenColor range:NSMakeRange(26, 10)];
+        [msg addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:13.f] range:NSMakeRange(0, msg.length)];
+        
+        _blinkLabel.fontSize = 15;
+        _blinkLabel.foregroundColor = UIColor.whiteColor.CGColor;
+        _blinkLabel.string = msg;
+    } else{
+        _blinkLabel.fontSize = fontSize;
+        _blinkLabel.foregroundColor = color;
+        _blinkLabel.string = message;
+    }
+}
+
+- (void)displayBlinkMessage {
+    self.blinkLabel = [CATextLayer layer];
+    _blinkLabel.frame = [self getBlinkMessageRect];
+    _blinkLabel.contentsScale = [UIScreen mainScreen].scale;
+    _blinkLabel.alignmentMode = kCAAlignmentCenter;
+    _blinkLabel.foregroundColor = UIColor.whiteColor.CGColor;
+    [self addMessage];
+    [_videoPreviewLayer addSublayer:_blinkLabel];
 }
 
 - (CGRect)getBlinkMessageRect {
     CGFloat width = 330.f;
-    CGFloat height = 60.f;
+    CGFloat height = 55.f;
     CGRect mainViewFrame = [self getViewFrame];
-
-    return CGRectMake(mainViewFrame.origin.x + mainViewFrame.size.width / 2.f -width / 2.f,
-                      0.08f * mainViewFrame.size.height,
-                      width,
-                      height);
-}
-
-- (CGRect)videoBox:(CGSize)frameSize apertureSize:(CGSize)apertureSize {
-//    CGFloat apertureRatio = apertureSize.height / apertureSize.width;
-//    CGFloat viewRatio = frameSize.width / frameSize.height;
-
-    CGSize size = CGSizeMake(apertureSize.height * (frameSize.height / apertureSize.width), frameSize.height);
-    CGRect videoBox = CGRectMake(.0, .0, size.width, size.height);
     
-    if (size.width < frameSize.width) {
-        videoBox.origin.x = (frameSize.width - size.width) / 2.0f;
-    } else {
-        videoBox.origin.x = (size.width - frameSize.width) / 2.0f;
-    }
-
-    if (size.height < frameSize.height) {
-        videoBox.origin.y = (frameSize.height - size.height) / 2.0f;
-    } else {
-        videoBox.origin.y = (size.height - frameSize.height) / 2.0f;
-    }
-
-    return videoBox;
+    return CGRectMake(mainViewFrame.origin.x + mainViewFrame.size.width/2.f-width/2.f,
+                      0.06f*mainViewFrame.size.height, width, height);
 }
-
-- (CGRect)calculateFaceRect:(CGRect)faceBounds clearAperture:(CGRect)clearAperture {
-    CGSize parentFrameSize = self.videoPreviewLayer.frame.size;
-    CGSize apperatureSize = clearAperture.size;
-    if (@available(iOS 11.0, *)) {
-        parentFrameSize.width = parentFrameSize.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right;
-        apperatureSize.width = apperatureSize.width - self.view.safeAreaInsets.left - self.view.safeAreaInsets.right;
-    } else {
-        // Fallback on earlier versions
-    }
-    
-    if (@available(iOS 11.0, *)) {
-        parentFrameSize.height = parentFrameSize.height - self.view.safeAreaInsets.top - self.view.safeAreaInsets.bottom;
-        apperatureSize.height = apperatureSize.height - self.view.safeAreaInsets.top - self.view.safeAreaInsets.bottom;
-    } else {
-        // Fallback on earlier versions
-    }
-    CGRect previewBox = [self videoBox:parentFrameSize apertureSize:apperatureSize];
-    CGRect faceRect = faceBounds;
-
-    [self swap:&faceRect.size.width second:&faceRect.size.height];
-    [self swap:&faceRect.origin.x second:&faceRect.origin.y];
-
-    CGFloat widthScaleBy = previewBox.size.width / apperatureSize.height;
-    CGFloat heightScaleBy = previewBox.size.height / apperatureSize.width;
-    faceRect.size.width *= widthScaleBy;
-    faceRect.size.height *= heightScaleBy;
-    faceRect.origin.x *= widthScaleBy;
-    faceRect.origin.y *= heightScaleBy;
-
-    faceRect = CGRectOffset(faceRect, .0f, previewBox.origin.y);
-    CGRect frame = CGRectMake((parentFrameSize.width) - faceRect.origin.x - faceRect.size.width - previewBox.origin.x / 2.0,
-                              faceRect.origin.y, faceRect.size.width, faceRect.size.height);
-    return frame;
-}
-
-- (void)swap:(CGFloat *)first second:(CGFloat *)second {
-    // TODO: Check this
-    CGFloat tmp = *first;
-    *first = *second;
-    *second = tmp;
-}
-
 
 // MARK: - AcuantHGLiveFaceCaptureDelegate
 
-- (void)liveFaceDetailsCapturedWithLiveFaceDetails:(LiveFaceDetails *)liveFaceDetails {
-    
-    if (liveFaceDetails.faceRect && liveFaceDetails.image && liveFaceDetails.cleanAperture) {
-        CGRect translatedFaceRect = [self calculateFaceRect:liveFaceDetails.faceRect.toCGRect clearAperture:liveFaceDetails.cleanAperture.toCGRect];
-        CGFloat topPadding = self.view.safeAreaInsets.top;
-        CGFloat bottomPadding = self.view.safeAreaInsets.bottom;
-        CGFloat width = 1.1f * translatedFaceRect.size.width;
-        CGFloat height = 1.3f * translatedFaceRect.size.height;
-        CGFloat x = translatedFaceRect.origin.x + (translatedFaceRect.size.width - width) + (topPadding + bottomPadding) / 2.f;
-        CGFloat y = translatedFaceRect.origin.y + (translatedFaceRect.size.height - height);
-        CGRect faceRect = CGRectMake(x, y, width, height);
-        [_faceOval removeFromSuperlayer];
+- (void)liveFaceDetailsCapturedWithLiveFaceDetails:(LiveFaceDetails *)liveFaceDetails
+                                          faceType:(enum AcuantFaceType)faceType {
+    if ([self shouldSkipFrame:liveFaceDetails faceType:faceType]) {
+        return;
+    }
+
+    switch (faceType) {
+        case AcuantFaceTypeNONE:
+            [self addMessage];
+            break;
+        case AcuantFaceTypeFACE_TOO_CLOSE:
+            [self addMessage:@"Too Close! Move Away"];
+            break;
+        case AcuantFaceTypeFACE_TOO_FAR:
+            [self addMessage:@"Move Closer"];
+            break;
+        case AcuantFaceTypeFACE_NOT_IN_FRAME:
+            [self addMessage:@"Move in Frame"];
+            break;
+        case AcuantFaceTypeFACE_GOOD_DISTANCE:
+            [self addMessage:@"Blink!" color:UIColor.greenColor.CGColor];
+            break;
+        case AcuantFaceTypeFACE_MOVED:
+            [self addMessage:@"Hold Steady"];
+            break;
+    }
+
+    if (liveFaceDetails.faceRect && liveFaceDetails.cleanAperture) {
+        CGRect rect = liveFaceDetails.faceRect.toCGRect;
+        CGRect totalSize = liveFaceDetails.cleanAperture.toCGRect;
+        CGRect scaled = CGRectMake((rect.origin.x - 150)/totalSize.size.width,
+                                   1-((rect.origin.y)/totalSize.size.height + (rect.size.height)/totalSize.size.height),
+                                   (rect.size.width + 150)/totalSize.size.width, (rect.size.height)/totalSize.size.height);
+        CGRect faceRect = [_videoPreviewLayer rectForMetadataOutputRectOfInterest:scaled];
         
-        self.faceOval = [CAShapeLayer new];
-        _faceOval.path = [UIBezierPath bezierPathWithOvalInRect:faceRect].CGPath;
-        _faceOval.fillColor = UIColor.clearColor.CGColor;
-        _faceOval.strokeColor = UIColor.greenColor.CGColor;
-        _faceOval.lineWidth = 5.f;
-        [_videoPreviewLayer addSublayer:_faceOval];
-        if (liveFaceDetails.isLiveFace) {
-            if (!_captured) {
-                _captured = YES;
-            } else if (_delegate) {
-                [_delegate liveFaceCapturedWithImage:liveFaceDetails.image];
-                _delegate = nil;
-            }
+        _faceOval.hidden = NO;
+        _faceOval.path = [UIBezierPath bezierPathWithRect:faceRect].CGPath;
+
+        if (liveFaceDetails.isLiveFace && !_captured) {
+            _captured = YES;
+            [_delegate liveFaceCapturedWithImage:liveFaceDetails.image];
         }
-    } else if (!liveFaceDetails || !liveFaceDetails.faceRect) {
-        [self.faceOval removeFromSuperlayer];
+    } else if(!liveFaceDetails || !liveFaceDetails.faceRect) {
+        _faceOval.hidden = YES;
     }
 }
 
